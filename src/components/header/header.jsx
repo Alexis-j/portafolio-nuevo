022e29f4a8b6aa2703f23158f5d3c9ee201b1bfd
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-01-31 11:02:11 +0100

    header listo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,21 @@
-// Header.jsx
-import React, { useState } from 'react';
-import Logo from './logo'; // Importa el componente Logo
+import React, { useState, useEffect } from 'react';
+import Logo from './logo';
+import NavListDrawer from './navListDrawer'; // Importa el componente NavListDrawer
 import '../../styles/header.css';
 
 function Header() {
   const [menuBackdropStyle, setMenuBackdropStyle] = useState({});
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkWindowSize = () => {
+      setIsMobile(window.innerWidth <= 768); // Ajusta el valor según tu criterio de pantalla pequeña
+    };
+
+    checkWindowSize();
+    window.addEventListener('resize', checkWindowSize);
+    return () => window.removeEventListener('resize', checkWindowSize);
+  }, []);
 
   const handleHover = (e) => {
     const { left, top, width, height } = e.target.getBoundingClientRect();
@@ -13,34 +24,35 @@ function Header() {
       top: `${top}px`,
       width: `${width}px`,
       height: `${height}px`,
-      opacity: 1, // Comienza con opacidad 0
+      opacity: 1,
       visibility: 'visible',
     });
   };
 
-
-
-const handleLeave = (e) => {
-  setMenuBackdropStyle({
-    opacity: 0,
-    visibility: 'hidden', // Mantén la visibilidad mientras se aplica la transición
-  });
-};
+  const handleLeave = () => {
+    setMenuBackdropStyle({
+      opacity: 0,
+      visibility: 'hidden',
+    });
+  };
 
   return (
     <header className="header" onMouseLeave={handleLeave}>
       <Logo />
-      <nav className="nav-menu" >
-        <ul id="nav-menu" className="nav-link">
-          <li><a href="/" onMouseEnter={handleHover} >About Me</a></li>
-          <li><a href="/" onMouseEnter={handleHover} >Projects</a></li>
-          <li><a href="/" onMouseEnter={handleHover} >Contact</a></li>
-        </ul>
-      </nav>
-
+      {isMobile ? (
+        <NavListDrawer />
+      ) : (
+        <nav className="nav-menu">
+          <ul id="nav-menu" className="nav-link">
+            <li><a href="/" onMouseEnter={handleHover}>About Me</a></li>
+            <li><a href="/" onMouseEnter={handleHover}>Projects</a></li>
+            <li><a href="/" onMouseEnter={handleHover}>Contact</a></li>
+          </ul>
+        </nav>
+      )}
       <div
         id="menu-backdrop"
-        className='menu-backdrop'
+        className="menu-backdrop"
         style={menuBackdropStyle}
       ></div>
     </header>
--- src/components/header/navListDrawer.jsx
+++ src/components/header/navListDrawer.jsx
@@ -1,29 +1,57 @@
-// NavListDrawer.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/navListDrawer.css';
 
 function NavListDrawer() {
-  const [showMenu, setShowMenu] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
     setIsMenuOpen(!isMenuOpen);
+    setShowLinks(!showLinks);
   };
 
+  useEffect(() => {
+    let timeout;
+    if (isMenuOpen) {
+      setShowLinks(true);
+      let delay = 0;
+
+      // Intervalo de medio segundo (500 milisegundos) para cada enlace
+      const links = document.querySelectorAll('.menu.show li');
+      links.forEach((link, index) => {
+        delay += 150; // Aumenta el retraso para cada enlace
+        timeout = setTimeout(() => {
+          link.style.opacity = 1;
+        }, delay);
+      });
+    } else {
+      setShowLinks(false);
+      const links = document.querySelectorAll('.menu.show li');
+      links.forEach((link) => {
+        link.style.opacity = 0;
+      });
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isMenuOpen]);
+
   return (
-    <nav className={`navbar mobile-navbar ${isMenuOpen ? 'show' : ''}`}>
-      <div className="menu-icon" onClick={toggleMenu}>
-        <div className={`menu-icon-line ${isMenuOpen ? 'open' : ''}`}></div>
-        <div className={`menu-icon-line ${isMenuOpen ? 'open' : ''}`}></div>
-        <div className={`menu-icon-line ${isMenuOpen ? 'open' : ''}`}></div>
+    <div className="nav-list-drawer">
+      <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="menu-icon-line"></div>
+        <div className="menu-icon-line"></div>
+        <div className="menu-icon-line"></div>
       </div>
-      <ul className="menu">
-        <li><a href="/about" className="menu-link">About Me</a></li>
-        <li><a href="/contact" className="menu-link">Projects</a></li>
-        <li><a href="/contact" className="menu-link">Contact</a></li>
-      </ul>
-    </nav>
+      {isMenuOpen && (
+        <div className="menu-container">
+          <ul className={`menu ${showLinks ? 'show' : ''}`}>
+            <li style={{ opacity: showLinks ? 0 : 1 }}><a href="/about" className="menu-link">About Me</a></li>
+            <li style={{ opacity: showLinks ? 0 : 1 }}><a href="/projects" className="menu-link">Projects</a></li>
+            <li style={{ opacity: showLinks ? 0 : 1 }}><a href="/contact" className="menu-link">Contact</a></li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
